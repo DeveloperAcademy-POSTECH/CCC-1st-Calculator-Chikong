9a3e48d73c4e20ebeda54d430a3264b81a8e23d4
jeohong <jeohong59@gmail.com>
2022-05-27 16:13:56 +0900

    Initial Commit

---
+++ Calculator_Clone/Calculator_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_CloneApp.swift
+//  Calculator_Clone
+//
+//  Created by Hong jeongmin on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
