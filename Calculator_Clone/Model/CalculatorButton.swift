98f8003932b375639b901c726b655641eef2e7c0
jeohong <jeohong59@gmail.com>
2022-05-29 13:02:48 +0900

    Update calculator Button 모델 색상 수정

--- Calculator_Clone/Model/CalculatorButton.swift
+++ Calculator_Clone/Model/CalculatorButton.swift
@@ -34,9 +34,27 @@ enum CalculatorButton: String{
         case .divide, .multiple, .minus, .plus, .equal:
             return .orange
         case .clear, .negative, .modular:
-            return Color(.lightGray)
+            return Color.calcuButtonColor
         default:
-            return Color(.darkGray)
+            return Color.numberButtonColor
         }
     }
+}
+
+extension Color {
+    static let numberButtonColor = Color(hex: "#333333")
+    static let calcuButtonColor = Color(hex: "#A5A5A5")
+    
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >>  8) & 0xFF) / 255.0
+        let b = Double((rgb >>  0) & 0xFF) / 255.0
+        self.init(red: r, green: g, blue: b)
+      }
 }
