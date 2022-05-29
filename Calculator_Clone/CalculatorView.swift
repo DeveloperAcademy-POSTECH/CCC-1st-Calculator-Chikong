c439eed127815e5bff443c1053fd3af1a645127a
jeohong <jeohong59@gmail.com>
2022-05-29 16:01:00 +0900

    Add Calculator UI 구현 ( 버튼 클릭시 색상변화 적용 X )

---
+++ Calculator_Clone/CalculatorView.swift
@@ -0,0 +1,136 @@
+//
+//  ContentView.swift
+//  Calculator_Clone
+//
+//  Created by Hong jeongmin on 2022/05/27.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    
+    @State private var result = "0"
+    
+    let calcuButton: [[CalculatorButton]] = [
+        [.clear, .negative, .modular, .divide],
+        [.seven, .eight, .nine, .multiple],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .dot, .equal]
+    ]
+    
+    var body: some View {
+        ZStack{
+            Color.black.ignoresSafeArea(.all)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Text(result)
+                        .foregroundColor(.white)
+                        .font(.system(size: 100))
+                        .fontWeight(.light)
+                }
+                .padding()
+                
+                VStack {
+                    ForEach(calcuButton, id:\.self) { row in
+                        HStack(spacing: 12) {
+                            ForEach(row, id:\.self) { buttonItem in
+                                Button(action: {
+                                    // 기능 구현
+                                }) {
+                                    RoundedRectangle(cornerRadius: 50)
+                                        .frame(width:calcuWidth(buttonItem), height: calcuHeight())
+                                        .foregroundColor(buttonItem.buttonColor)
+                                        .overlay(){
+                                            HStack{
+                                                buttonText(buttonItem)
+                                                    .frame(width: calcuTextWidth(buttonItem), height: calcuHeight(), alignment: buttonTextAlignment(buttonItem))
+                                                    .foregroundColor(buttonTextColor(buttonItem))
+                                                    .font(.system(size: 30))
+                                            }
+                                        }
+                                }
+                            }
+                        }
+                    }
+                }
+                .padding(.bottom, 30)
+            }
+        }
+    }
+    
+    // 버튼의 너비를 계산하는 함수
+    func calcuWidth(_ item: CalculatorButton) -> CGFloat {
+        switch item {
+        case .zero:
+            return ((UIScreen.main.bounds.width - (4 * 12)) / 4) * 2 + 5
+        default:
+            return (UIScreen.main.bounds.width - (5 * 12)) / 4
+        }
+    }
+    
+    // 버튼의 텍스트 너비를 계산하는 함수 ( 0 의 경우 라인을 맞추기 위하여 별도의 함수 생성 )
+    func calcuTextWidth(_ item: CalculatorButton) -> CGFloat {
+        switch item {
+        case .zero:
+            return ((UIScreen.main.bounds.width - 5 * 12)) / 3
+        default:
+            return (UIScreen.main.bounds.width - (5 * 12)) / 4
+        }
+    }
+    
+    // 버튼의 높이를 계산하는 함수
+    func calcuHeight() -> CGFloat {
+        return (UIScreen.main.bounds.width - (5 * 12)) / 4
+    }
+    
+    // 버튼의 글자를 설정하는 함수 ( SF 심볼의 경우와 일반 Text인 경우 )
+    func buttonText(_ item: CalculatorButton) -> Text {
+        switch item {
+        case .negative:
+            return Text(Image(systemName: "plus.forwardslash.minus"))
+        case .plus:
+            return Text(Image(systemName: "plus"))
+        case .minus:
+            return Text(Image(systemName: "minus"))
+        case .multiple:
+            return Text(Image(systemName: "multiply"))
+        case .divide:
+            return Text(Image(systemName: "divide"))
+        case .equal:
+            return Text(Image(systemName: "equal"))
+        case .modular:
+            return Text(Image(systemName: "percent"))
+        default:
+            return Text("\(item.rawValue)")
+        }
+    }
+    
+    // 버튼 내부 글씨를 설정하는 함수
+    func buttonTextColor(_ item: CalculatorButton) -> Color {
+        switch item {
+        case .clear, .negative, .modular:
+            return .black
+        default:
+            return .white
+        }
+    }
+    
+    // 버튼 내부 텍스트의 정렬을 계산
+    func buttonTextAlignment(_ item: CalculatorButton) -> Alignment {
+        switch item {
+        case .zero:
+            return .leading
+        default:
+            return .center
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView()
+    }
+}
